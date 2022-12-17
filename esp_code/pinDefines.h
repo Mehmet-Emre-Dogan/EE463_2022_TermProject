458545f7180629127f2bc932b1d0dab207db8890
Mehmet Emre Doğan <87245315+Mehmet-Emre-Dogan@users.noreply.github.com>
2022-12-17 19:42:29 +0300

    working on esp code

---
+++ esp_code/pinDefines.h
@@ -0,0 +1,8 @@
+#ifndef _pinDefines_h_
+#define _pinDefines_h_
+#define BTN D5
+#define INDICATOR_LED D6
+#define BLUE_LED LED_BUILTIN
+#define MOTOR_LEFT_PIN D1
+#define MOTOR_RIGHT_PIN D2
+#endif
---
+++ esp_code/secrets.h
@@ -0,0 +1,16 @@
+#ifndef _secrets_h_
+#define _secrets_h_
+/*############################################################################ Classified information ############################################################################*/
+//
+// Replace with your network credentials
+const char* ssid = "VoltMotors"; // Input your wifi network name
+const char* password = "do not type it here"; // Input your wifi password
+//
+/*############################################################################ Classified information ############################################################################*/
+const char* deviceName = "463Motor";
+WiFiServer webserver(80);// Set web server port number to 80
+IPAddress staticIP(192, 168, 1, 60); //ESP static ip, change according to your desire
+IPAddress gateway(192, 168, 1, 1);   //IP Address of your WiFi Router (Gateway)
+IPAddress subnet(255, 255, 255, 0);  //Subnet mask
+IPAddress dns(8, 8, 8, 8);  //DNS
+#endif
